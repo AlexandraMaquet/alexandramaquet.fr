3e1ac4f88c657a196157424abdfe2bb1d8e147fe
Alexandra Maquet <maquet.alexandra@hotmail.com>
2017-12-12 14:51:27 +0100

    essai commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+ /*  cheet;
+  constructor(){
+    this.cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+      alert('Voilà!');
+    });
+  } */
+  
+}
