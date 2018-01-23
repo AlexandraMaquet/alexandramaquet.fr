355c8b6e236f3bc7ee1c0a0a4cf6449c3cb1b320
Alexandra Maquet <maquet.alexandra@hotmail.com>
2018-01-23 13:56:06 +0100

    ajout lien malt dans contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
- /*  cheet;
-  constructor(){
-    this.cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-      alert('Voilà!');
-    });
-  } */
-  
+
+  /*  cheet;
+   constructor(){
+     this.cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+       alert('Voilà!');
+     });
+   } */
+
+  function(d, id) {
+    if (d.getElementById(id)) {
+       return;
+      }
+    const s = d.createElement('script');
+    const c = d.getElementsByTagName('script')[0];
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://widgets.malt.com/v1/js/sdk.min.js';
+    c.parentNode.insertBefore(s, c);
+  }
 }
